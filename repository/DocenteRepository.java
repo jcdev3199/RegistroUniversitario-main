2813f094e54d2d69472ad24f2e9405d67d6eafcd
jcdev3199 <168743953+jcdev3199@users.noreply.github.com>
2025-05-27 10:42:58 -0400

    Delete repository directory

--- repository/DocenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.universidad.repository;
-
-import com.universidad.model.Docente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocenteRepository extends JpaRepository<Docente, Long> {
-    // Puedes agregar métodos personalizados si lo necesitas
-}
--- repository/InscripcionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.universidad.repository;
-
-import com.universidad.model.Inscripcion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
-}
